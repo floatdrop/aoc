7d72cdd61d6747fb569a2a7529759742e89f4e01
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-01 16:59:14 +0500

    Type deduction

--- src/day1.rs
+++ src/day1.rs
@@ -5,20 +5,14 @@ use itertools::Itertools;
 static INPUT: &str = std::include_str!("day1.txt");
 
 pub fn part1() -> i64 {
-    let numbers = INPUT.lines()
-        .map(|l| l.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>();
-
+    let numbers = INPUT.lines().map(|l| l.parse::<i64>().unwrap()).collect::<Vec<_>>();
     let pair = numbers.iter().tuple_combinations().filter(|&(a, b)| a + b == 2020).next().unwrap();
 
     pair.0 * pair.1
 }
 
 pub fn part2() -> i64 {
-    let numbers = INPUT.lines()
-        .map(|l| l.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>();
-
+    let numbers = INPUT.lines().map(|l| l.parse::<i64>().unwrap()).collect::<Vec<_>>(); 
     let tuple = numbers.iter().tuple_combinations().filter(|&(a, b, c)| a + b + c == 2020).next().unwrap();
 
     tuple.0 * tuple.1 * tuple.2
