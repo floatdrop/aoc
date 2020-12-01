ad4d0af876e4ddcac44e177ac04b2e242f05aa38
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-01 22:58:07 +0500

    Add optimized versions of day_1

--- benches/day1.rs
+++ benches/day1.rs
@@ -4,7 +4,9 @@ use aoc::day1;
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("day1::part1", |b| b.iter(|| day1::part1()));
+    c.bench_function("day1::part1_opt", |b| b.iter(|| day1::part1_opt()));
     c.bench_function("day1::part2", |b| b.iter(|| day1::part2()));
+    c.bench_function("day1::part2_opt", |b| b.iter(|| day1::part2_opt()));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/day1.rs
+++ src/day1.rs
@@ -18,6 +18,40 @@ pub fn part2() -> i64 {
     tuple.0 * tuple.1 * tuple.2
 }
 
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+pub fn part1_opt() -> i64 {
+    let numbers = INPUT.lines().map(|l| l.parse::<i64>().unwrap()).collect::<Vec<_>>();
+    let set = HashSet::<i64>::from_iter(numbers.iter().cloned());
+
+    for number in numbers {
+        let diff = 2020 - number;
+        if set.contains(&diff) {
+            return number * diff;   
+        }
+    }
+    
+    unreachable!();
+}
+
+pub fn part2_opt() -> i64 {
+    let numbers = INPUT.lines().map(|l| l.parse::<i64>().unwrap()).collect::<Vec<_>>();
+    let set = HashSet::<i64>::from_iter(numbers.iter().cloned());
+
+    for (i, a) in numbers.iter().enumerate() {
+        for b in numbers[i + 1..].iter() {
+            let c = 2020 - (a + b);
+            if set.contains(&c) {
+                return a * b * c;   
+            }
+        }
+    }
+    
+    unreachable!();
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +64,15 @@ mod tests {
     #[test]
     fn check_part2_answer() {
         assert_eq!(part2(), 289270976);
+    }
+
+    #[test]
+    fn check_part1_opt_answer() {
+        assert_eq!(part1_opt(), 692916);
+    }
+
+    #[test]
+    fn check_part2_opt_answer() {
+        assert_eq!(part2_opt(), 289270976);
     }
 }
