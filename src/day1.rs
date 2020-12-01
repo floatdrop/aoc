0bf4999737d7355b33c522cb6484cb63acfbec7d
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-01 12:15:56 +0500

    Fix edition and remove filter (because there maybe negative numbers)

--- src/day1.rs
+++ src/day1.rs
@@ -7,7 +7,6 @@ static INPUT: &str = std::include_str!("day1.txt");
 pub fn part1() -> i64 {
     let numbers = INPUT.lines()
         .map(|l| l.parse::<i64>().unwrap())
-        .filter(|&i| i <= 2020)
         .collect::<Vec<i64>>();
 
     let pair = numbers.iter().tuple_combinations().filter(|&(a, b)| a + b == 2020).next().unwrap();
@@ -18,7 +17,6 @@ pub fn part1() -> i64 {
 pub fn part2() -> i64 {
     let numbers = INPUT.lines()
         .map(|l| l.parse::<i64>().unwrap())
-        .filter(|&i| i <= 2020)
         .collect::<Vec<i64>>();
 
     let tuple = numbers.iter().tuple_combinations().filter(|&(a, b, c)| a + b + c == 2020).next().unwrap();
