c011b858215e3b649d4c1c60f26513408e5e5cb8
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-02 22:17:06 +0500

    Rewrite this 💩

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use aoc::day1;
+use aoc::day2;
 
 fn main() {
-    println!("Day 1 (part 1): {}", day1::part1());
-    println!("Day 1 (part 2): {}", day1::part2());
+    println!("Day 2 (part 1): {}", day2::part1());
+    println!("Day 2 (part 2): {}", day2::part2());
 }
