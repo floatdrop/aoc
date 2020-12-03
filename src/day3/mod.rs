f27ed3bcd1ee2de5b3b005675b228572dcafb5b5
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-03 10:23:32 +0500

    Add day3 solution

---
+++ src/day3/mod.rs
@@ -0,0 +1,55 @@
+static INPUT: &str = std::include_str!("input.txt");
+
+pub fn count_trees(right: usize, down: usize) -> usize {
+    let rows: Vec<&str> = INPUT.lines().collect();
+    
+    let width = rows.iter().next().unwrap().len();
+    let height = rows.len();
+
+    let mut x = 0;
+    let mut y = 0;
+    let mut count = 0;
+
+    while y < height {
+        if rows[y].chars().nth(x).unwrap() == '#' {
+            count += 1;
+        }
+
+        x += right;
+        y += down;
+
+        if x >= width {
+            x = x - width
+        }
+    }
+
+    count
+
+}
+
+pub fn part1() -> usize {
+    count_trees(3, 1)
+}
+
+pub fn part2() -> usize {
+    count_trees(1, 1) * 
+    count_trees(3, 1) * 
+    count_trees(5, 1) * 
+    count_trees(7, 1) * 
+    count_trees(1, 2) 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_part1_answer() {
+        assert_eq!(part1(), 265);
+    }
+
+    #[test]
+    fn check_part2_answer() {
+        assert_eq!(part1(), 3154761400);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day2;
+mod day3;
 
 fn main() {
-    println!("Day 2 (part 1): {}", day2::part1());
-    println!("Day 2 (part 2): {}", day2::part2());
+    println!("Day 3 (part 1): {}", day3::part1());
+    println!("Day 3 (part 2): {}", day3::part2());
 }
