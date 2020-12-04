b0bfc6f34ed461bd4335110fd9be67c8e21c3fc3
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-04 16:50:57 +0500

    Simplify code

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,60 +1,35 @@
 use regex::Regex;
 use lazy_static::lazy_static;
-use itertools::Itertools;
-use itertools::join;
 
 static INPUT: &str = std::include_str!("input.txt");
 
 pub fn part1() -> usize {
-    let rows: Vec<&str> = INPUT.lines().collect();
-
-    let mut count = 0;
-
-    for (_, g) in rows.iter().group_by(|&&l| l == "").into_iter() {
-        let s = join(g, " ");
-
-
-        if s == "" {
-            continue;
-        }
-
-        if s.contains("byr:") && s.contains("iyr:") && s.contains("eyr:")
+    INPUT.split("\n\n").filter(|s| {
+        s.contains("byr:") && s.contains("iyr:") && s.contains("eyr:")
             && s.contains("hgt:") && s.contains("hcl:") && s.contains("ecl:")
-                && s.contains("pid:") {
-            count += 1
-        }
-    }
-
-    count
+                && s.contains("pid:")
+    }).count()
 }
 
 pub fn part2() -> usize {
     lazy_static! {
-        static ref BYR_RE: Regex = Regex::new(r"byr:(\d{4}) ").unwrap();
-        static ref IYR_RE: Regex = Regex::new(r"iyr:(\d{4}) ").unwrap();
-        static ref EYR_RE: Regex = Regex::new(r"eyr:(\d{4}) ").unwrap();
-        static ref HGT_RE: Regex = Regex::new(r"hgt:(\d{2,3})(cm|in) ").unwrap();
-        static ref HCL_RE: Regex = Regex::new(r"hcl:#([a-z0-9]{6}) ").unwrap();
-        static ref ECL_RE: Regex = Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth) ").unwrap();
-        static ref PID_RE: Regex = Regex::new(r"pid:(\d{9}) ").unwrap();
+        static ref BYR_RE: Regex = Regex::new(r"byr:(\d{4})\b").unwrap();
+        static ref IYR_RE: Regex = Regex::new(r"iyr:(\d{4})\b").unwrap();
+        static ref EYR_RE: Regex = Regex::new(r"eyr:(\d{4})\b").unwrap();
+        static ref HGT_RE: Regex = Regex::new(r"hgt:(\d{2,3})(cm|in)\b").unwrap();
+        static ref HCL_RE: Regex = Regex::new(r"hcl:#([a-z0-9]{6})\b").unwrap();
+        static ref ECL_RE: Regex = Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\b").unwrap();
+        static ref PID_RE: Regex = Regex::new(r"pid:(\d{9})\b").unwrap();
     }
 
-    let rows: Vec<&str> = INPUT.lines().collect();
+    let passports = INPUT.split("\n\n");
 
     let mut count = 0;
 
-    for (_, g) in rows.iter().group_by(|&&l| l == "").into_iter() {
-        let mut s = join(g, " ");
-
-        if s == "" {
-            continue;
-        }
-
-        s += " ";
-
+    for s in passports {
         if let Some(c) = BYR_RE.captures(&s) {
             let byr = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-            if byr < 1920 || byr > 2002 {
+            if !(1920..=2002).contains(&byr) {
                 continue;
             }
         } else {
@@ -63,7 +38,7 @@ pub fn part2() -> usize {
 
         if let Some(c) = IYR_RE.captures(&s) {
             let iyr = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-            if iyr < 2010 || iyr > 2020 {
+            if !(2010..=2020).contains(&iyr) {
                 continue;
             }
         } else {
@@ -72,7 +47,7 @@ pub fn part2() -> usize {
 
         if let Some(c) = EYR_RE.captures(&s) {
             let eyr = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
-            if eyr < 2020 || eyr > 2030 {
+            if !(2020..=2030).contains(&eyr) {
                 continue;
             }
         } else {
@@ -82,10 +57,10 @@ pub fn part2() -> usize {
         if let Some(c) = HGT_RE.captures(&s) {
             let hgt = c.get(1).unwrap().as_str().parse::<i64>().unwrap();
             let units = c.get(2).unwrap().as_str();
-            if units == "cm" && (hgt < 150 || hgt > 193) {
+            if units == "cm" && !(150..=193).contains(&hgt) {
                 continue;
             }
-            if units == "in" && (hgt < 59 || hgt > 76) {
+            if units == "in" && !(59..=76).contains(&hgt) {
                 continue;
             }
         } else {
