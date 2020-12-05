cf5ae81be437e06007b0467362e842636c4462bf
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-05 10:38:24 +0500

    Simplify matcher

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -4,35 +4,27 @@ static INPUT: &str = std::include_str!("input.txt");
 
 pub fn part1() -> usize {
     INPUT.lines().map(|s| {
-        let pos = s.chars().fold((0, 8, 0, 128), |acc, c| {
+        s.chars().fold(0, |acc, c| {
             match c {
-                'F' => { (acc.0, acc.1, acc.2, (acc.2 + acc.3) / 2) },
-                'B' => { (acc.0, acc.1, (acc.2 + acc.3) / 2, acc.3) },
-                'L' => { (acc.0, (acc.0 + acc.1) / 2, acc.2, acc.3) },
-                'R' => { ((acc.0 + acc.1) / 2, acc.1, acc.2, acc.3) },
+                'F' | 'L' => { acc << 1 },
+                'B' | 'R' => { acc << 1 | 1 },
                 _ => unreachable!(),
             }
-        });
-        (pos.0, pos.2)
+        })
     })
-    .map(|(x, y)| y * 8 + x)
     .max().unwrap()
 }
 
 pub fn part2() -> usize {
     let ids: HashSet<usize> = INPUT.lines().map(|s| {
-        let pos = s.chars().fold((0, 8, 0, 128), |acc, c| {
+        s.chars().fold(0, |acc, c| {
             match c {
-                'F' => { (acc.0, acc.1, acc.2, (acc.2 + acc.3) / 2) },
-                'B' => { (acc.0, acc.1, (acc.2 + acc.3) / 2, acc.3) },
-                'L' => { (acc.0, (acc.0 + acc.1) / 2, acc.2, acc.3) },
-                'R' => { ((acc.0 + acc.1) / 2, acc.1, acc.2, acc.3) },
+                'F' | 'L' => { acc << 1 },
+                'B' | 'R' => { acc << 1 | 1 },
                 _ => unreachable!(),
             }
-        });
-        (pos.0, pos.2)
+        })
     })
-    .map(|(x, y)| y * 8 + x)
     .collect();
 
     let all: HashSet<usize> = (100..900).collect();
