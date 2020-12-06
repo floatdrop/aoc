18af7f7d71f0b8dca023ed3ea386466649afd41c
Vsevolod Strukchinskii <floatdrop@gmail.com>
2020-12-06 18:22:29 +0500

    Vars, vars never changes.

--- benches/main.rs
+++ benches/main.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use aoc::day5;
+use aoc::day6;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("day5::part1", |b| b.iter(|| day5::part1()));
-    c.bench_function("day5::part2", |b| b.iter(|| day5::part2()));
+    c.bench_function("day6::part1", |b| b.iter(|| day6::part1()));
+    c.bench_function("day6::part2", |b| b.iter(|| day6::part2()));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -18,17 +18,17 @@ pub fn part1() -> usize {
 pub fn part2() -> usize {
     INPUT
         .split("\n\n")
-        .map(|g| {
-            let people = g.lines().count();
-            g.chars()
-             .filter(|c| ('a'..='z').contains(c))
-             .fold(HashMap::new(), |mut acc, c| {
-                *acc.entry(c).or_insert(0) += 1;
-                acc
-             })
-             .iter()
-             .filter(|&(_, &v)| v == people)
-             .count()
+        .map(|group| {
+            let people = group.lines().count();
+            group.chars()
+                .filter(|c| ('a'..='z').contains(c))
+                .fold(HashMap::new(), |mut acc, c| {
+                    *acc.entry(c).or_insert(0) += 1;
+                    acc
+                })
+                .iter()
+                .filter(|&(_, &v)| v == people)
+                .count()
         })
         .sum()
 }
